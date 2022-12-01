b8f0705bb4aa2cebb6a731814add403040d83e19
Ghulam Murtaza <w3ghulam@gmail.com>
2022-12-01 20:41:14 +0500

    Simple Nft with Reaveal function : WIP

--- contracts/simpleNFtReveal.sol
+++ contracts/simpleNFtReveal.sol
@@ -4,6 +4,7 @@
     2. And a wallet can have only 3 nfts
     3. And in one transaction they can mint 3 nfts
     4. And these should be dynamic, owner can change after that
+    5. And owner can mint 10 nfts as guest for its team members
 */
 
 // SPDX-License-Identifier: MIT
@@ -12,29 +13,25 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract simpleNFTReveal is ERC721Enumerable, Ownable {
+contract simpleNFTGift is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     string baseURI;
     string public baseExtension = ".json";
 
     uint256 public maxMintAmount = 3;
-    uint256 public maxSupply = 10; //total Mintables Supply
+    uint256 public maxSupply = 20; //total
     uint256 public cost = 0.01 ether;
+    uint256 public teamSupply = 10;
 
     bool public mintState = false;
-
-    bool public revealed = false;
-    string public notRevealedUri;
 
     constructor(
         string memory name_,
         string memory symbol_,
-        string memory _initBaseURI,
-        string memory _initNotRevealedUri
+        string memory _initBaseURI
     ) ERC721(name_, symbol_) {
-        baseURI = _initBaseURI;
-        notRevealedUri = _initNotRevealedUri;
+        setBaseURI(_initBaseURI);
     }
 
     // internal functions
@@ -42,7 +39,7 @@ contract simpleNFTReveal is ERC721Enumerable, Ownable {
         return baseURI;
     }
 
-    // external functions
+    // public functions
     function mint(uint256 _mintAmount) external payable {
         uint256 supply = totalSupply();
         require(mintState, "Minting is paused");
@@ -57,13 +54,43 @@ contract simpleNFTReveal is ERC721Enumerable, Ownable {
             "You cannot mint more than max NFTs"
         );
         require(
-            supply + _mintAmount <= maxSupply,
+            supply + _mintAmount + teamSupply <= maxSupply,
             "Cannot mint more than max Supply"
         );
 
         require(msg.value >= cost * _mintAmount, "Cost Error");
         for (uint256 i = 1; i <= _mintAmount; i++) {
             _mint(msg.sender, supply + i);
+        }
+    }
+
+    function gift(uint256 _mintAmount, address receiver)
+        external
+        payable
+        onlyOwner
+    {
+        uint256 supply = totalSupply();
+        require(mintState, "Minting is paused");
+        require(teamSupply > 0, "All Gift are dispatched");
+        require(_mintAmount <= teamSupply, "Cannot mint this amount as gift");
+        require(_mintAmount > 0, "Mint amount Cannot be zero");
+        require(
+            _mintAmount <= maxMintAmount,
+            "Cannot mint more than max mint amount"
+        );
+        require(
+            balanceOf(receiver) + _mintAmount <= maxMintAmount,
+            "You cannot mint more than max NFTs for this wallet"
+        );
+        require(
+            supply + _mintAmount <= maxSupply,
+            "Cannot mint more than max Supply"
+        );
+
+        require(msg.value >= cost * _mintAmount, "Cost Error");
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _mint(receiver, supply + i);
+            teamSupply--;
         }
     }
 
@@ -78,11 +105,7 @@ contract simpleNFTReveal is ERC721Enumerable, Ownable {
             _exists(tokenId),
             "ERC721Metadata: URI query for nonexistent token"
         );
-
-        if (revealed == false) {
-            return notRevealedUri;
-        }
-
+        // Only Owner Functions
         string memory currentBaseURI = _baseURI();
         return
             bytes(currentBaseURI).length > 0
@@ -110,39 +133,30 @@ contract simpleNFTReveal is ERC721Enumerable, Ownable {
     }
 
     //only owner
-
-    function reveal() public onlyOwner {
-        revealed = true;
-    }
-
-    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-        notRevealedUri = _notRevealedURI;
-    }
-
-    function setCost(uint256 _newCost) external onlyOwner {
+    function setCost(uint256 _newCost) public onlyOwner {
         cost = _newCost;
     }
 
-    function setmaxMintAmount(uint256 _newmaxMintAmount) external onlyOwner {
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
         maxMintAmount = _newmaxMintAmount;
     }
 
-    function setBaseURI(string memory _newBaseURI) external onlyOwner {
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
 
     function setBaseExtension(string memory _newBaseExtension)
-        external
+        public
         onlyOwner
     {
         baseExtension = _newBaseExtension;
     }
 
-    function setMintState(bool _state) external onlyOwner {
+    function setMintState(bool _state) public onlyOwner {
         mintState = _state;
     }
 
-    function withdraw() external payable onlyOwner {
+    function withdraw() public payable onlyOwner {
         require(address(this).balance > 0, "Balance of this Contract is Zero");
         (bool transfer, ) = payable(owner()).call{value: address(this).balance}(
             ""
